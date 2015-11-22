11d53687dd3356ec682f69f115dec14172bc9d26
Sascha Wombacher <sascha.wombacher@gmx.de>
2015-11-22 02:44:53 +0100

    small adjustments

--- examples/terrain.cpp
+++ examples/terrain.cpp
@@ -5,23 +5,18 @@
 
 
 std::mt19937 randomGen(std::random_device().operator()());
-
 class MyWindow : public cf::Window3D{
 public:
-    MyWindow(int* argc, char** argv, cv::Mat heightMap) : cf::Window3D(argc, argv), m_HeightMap(heightMap){
-        this->m_CameraAdjustment = 10.f;
-        this->m_AngleAdjustment = 10.f;
-    }
+    MyWindow(int* argc, char** argv, cv::Mat heightMap) : cf::Window3D(argc, argv), m_HeightMap(heightMap){ }
     virtual void draw() override{
         this->clear();
         const float minHeight =  0.1f;
         const float maxHeight = 10.f;
         const float cubeSize  =  0.5f;
-        std::uniform_real_distribution<float> dis(0.f, 1.f);
+        const std::uniform_real_distribution<float> dis(0.f, 1.f);
 
         for (int y = 0; y < this->m_HeightMap.rows; ++y){
         for (int x = 0; x < this->m_HeightMap.cols; ++x){
-
             glPushMatrix();
             {
                 float tmp = this->m_HeightMap.at<uchar>(y, x);
@@ -43,15 +38,13 @@ private:
     cv::Mat m_HeightMap;
 };
 
-
-
 int main(int argc, char** argv){
     if (argc < 2){
         std::cout << "Please provide a heightmap";
         return -1;
     }
     cv::Mat img = cv::imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-    cv::resize(img, img, cv::Size(300, 200));
+    cv::resize(img, img, cv::Size(255, 255));
     cv::imshow("HeightMap", img);
     cv::waitKey(1000);
 
