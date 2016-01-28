8cd34fcb78e3fe4cee8a053bece90f50d207d015
Bloody Angel <sascha.wombacher@gmx.de>
2016-01-28 11:47:21 +0000

    removed "return by reference" for get function in window2D class (visual++ doesn't work correctly in this case...), added support for debug/release libs in windows, added windows libs

--- examples/window_2D_example.cpp
+++ examples/window_2D_example.cpp
@@ -4,8 +4,9 @@
 
 
 int main(int argc, char** argv) {
-
     cf::Window2D w;
+	w.waitKey();
+
     for (int y = 0; y < w.getImageHeight(); ++y){
         for (int x = 0; x < w.getImageWidth() / 2;  ++x)
             w.setColor(x, y, cf::Color(255, 255, 255));
@@ -14,20 +15,30 @@ int main(int argc, char** argv) {
             w.setColor(x, y, cf::Color::BLUE);
     }
     w.show();
+	w.waitKey();
 
     const int step_count = 100;
     while(w.getColor(0, 0) == cf::Color::WHITE){
         for (int i = 0; i < step_count; ++i){
+			printf("Processing: %d%% \r", int((float)i / (float)step_count * 100));
             for (int y = 0; y < w.getImageHeight()   ; ++y){
             for (int x = 0; x < w.getImageWidth() - 1; ++x){
-                cf::Color& col = w.getColor(x + 1, y);
+				cf::Color col = w.getColor(x + 1, y);
                 w.setColor(x, y, col);
 
                 // alternative (and faster) version
                 // NOTE: by using this version you have to deal with y-axis inverting and invervall changing yourself
+				//		(additional you might have to know:
+				//			cv::Vec3b[0]		// blue  value
+				//			cv::Vec3b[1]		// green value
+				//			cv::Vec3b[2]		// red	 value
                 //
                 //cv::Mat& img = w.getImage();
-                //img.at<cf::Color>(y, x) = img.at<cf::Color>(y, x + 1);
+                //img.at<cv::Vec3b>(y, x) = img.at<cv::Vec3b>(y, x + 1);	// copy color
+				//
+				//img.at<cv::Vec3b>(y, x)[0] = img.at<cv::Vec3b>(y, x + 1)[0];	// copy blue
+				//img.at<cv::Vec3b>(y, x)[1] = img.at<cv::Vec3b>(y, x + 1)[1];	// copy green
+				//img.at<cv::Vec3b>(y, x)[2] = img.at<cv::Vec3b>(y, x + 1)[2];	// copy red
             }
             }
         }
