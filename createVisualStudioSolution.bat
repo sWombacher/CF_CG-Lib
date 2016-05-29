8996b91bfc0cbb43d6d3ce51b9460415a74ce2c5
Bloody Angel <sascha.wombacher@gmx.de>
2016-05-29 19:37:54 +0200

    simplified batch script, added VisualStudio to ignore list

--- createVisualStudioSolution.bat
+++ createVisualStudioSolution.bat
@@ -2,16 +2,3 @@
 mkdir VisualStudio
 cd VisualStudio
 ..\cmake\bin\cmake.exe -G "Visual Studio 14 Win64" ..
-
-set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
- 
-echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
-echo sLinkFile = ".\Project.lnk" >> %SCRIPT%
-echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.TargetPath = ".\VisualStudio\ALL_BUILD.vcxproj" >> %SCRIPT%
-echo oLink.WorkingDirectory =".\VisualStudio\" >> %SCRIPT%
-echo oLink.Save >> %SCRIPT%
- 
-cscript /nologo %SCRIPT%
- 
-del %SCRIPT%
