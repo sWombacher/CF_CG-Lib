f7caead1c3cb4ba23201a48a31a73df60966a8ad
Sascha Wombacher <sascha.wombacher@gmx.de>
2017-05-12 22:34:46 +0200

    moved hpp to header/src file

---
+++ src/windowRasterized.cpp
@@ -0,0 +1,15 @@
+#include "windowRasterized.h"
+
+namespace cf{
+
+cf::WindowRasterized::WindowRasterized(int width, int height, const char *windowName, const cf::Color &startColor)
+    : Window2D(width, height, windowName, startColor)
+{
+    this->setInvertYAxis(false);
+}
+
+cf::WindowRasterized::WindowRasterized(const char *filePath) : Window2D(filePath) {
+    this->setInvertYAxis(false);
+}
+
+}
