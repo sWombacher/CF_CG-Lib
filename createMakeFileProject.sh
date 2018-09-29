b4b23aaba343293670c79a3ed100f2098c7a0593
Sascha Wombacher <sascha.wombacher@gmx.de>
2018-09-29 11:50:48 +0200

    separate c++ and python scripts (Linux/macOS)

--- createMakeFileProject.sh
+++ createMakeFileProject.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 dir=MakeFileProject
-pythonDir="../PythonSolutions"
 
 mkdir -p $dir
 rm -r $dir/*
 cd $dir
-cmake -DCOMPILE_WITH_PYTHON_SUPPORT=ON -G "Unix Makefiles" ..
-make -j4
-
-# move all python related stuff into 'PythonVersion'
-mkdir -p $pythonDir
-cp ../lib/Debug/_cf.so swig/cf.py ../python/computerGeometry_example.py $pythonDir
-cp -r ../geometry_files/ $pythonDir
-
-echo To start the examples:
-echo "  - Change directory into 'PythonSolution'"
-echo "  - run 'python ./computerGeometry_example.py'"
-
+cmake -G "Unix Makefiles" ..
