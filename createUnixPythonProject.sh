a11f823434c3e613f896fea539255670de13e0fb
Bloody Angel <sascha.wombacher@gmx.de>
2020-07-16 15:04:17 +0200

    adjust unix python script to support cf subdir

--- createUnixPythonProject.sh
+++ createUnixPythonProject.sh
@@ -5,13 +5,15 @@ pythonDir="../PythonSolutions"
 mkdir -p $dir
 rm -r $dir/*
 cd $dir
-cmake -DCOMPILE_WITH_PYTHON_SUPPORT=ON -G "Unix Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=RELEASE -DCOMPILE_WITH_PYTHON_SUPPORT=ON -G "Unix Makefiles" ..
 make -j4
 
 # copy all python related stuff into 'PythonVersion'
-mkdir -p $pythonDir
-cp ../lib/Debug/_cf.so swig/cf.py ../python/*.py $pythonDir
+mkdir -p $pythonDir $pythonDir/cf
+cp ../lib/Release/_cf.so swig/cf.py $pythonDir/cf
 cp -r ../geometry_files ../chaos_files $pythonDir
+cp ../python/*.py $pythonDir/cf
+mv $pythonDir/helper.py $pythonDir/cf
 
 echo To start the examples:
 echo "  - Change directory into 'PythonSolution'"
